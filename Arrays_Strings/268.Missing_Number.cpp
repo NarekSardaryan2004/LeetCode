fddcfce44e787e37f3a420db9490ca994d364094
NarekSardaryan2004 <nareksardaryan2004@gmail.com>
2025-12-15 01:25:14 +0400

    Solution of Missing Number

---
+++ Arrays_Strings/268.Missing_Number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum1 = 0;
+        int sum2 = 0;
+
+        for (int i = 0; i < nums.size() + 1; ++i) {
+            sum1 += i;
+        }
+
+        for (int i : nums) {
+            sum2 += i;
+        }
+
+        return sum1 - sum2;
+    }
+};
